bb382095b363fb80b90e9780d68ee89d78a32fc7
dutch foy <jdutchfoy@gmail.com>
2023-04-10 20:57:41 -0700

    array-binary-search challenge 3

---
+++ python/code_challenges/array-binary-search/array_binary_search.py
@@ -0,0 +1,14 @@
+def BinarySearch(arr, key):
+    left, right = 0, len(arr) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if arr[mid] == key:
+            return mid
+        elif arr[mid] > key:
+            right = mid - 1
+        else:
+            left = mid + 1
+
+    return -1
