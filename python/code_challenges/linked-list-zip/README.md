1584ab1fd637959ae398e0995e9f81e232d864cd
dutch foy <jdutchfoy@gmail.com>
2023-04-16 14:45:24 -0700

    challenge 08 done

--- python/code_challenges/linked-list-zip/README.md
+++ python/code_challenges/linked-list-zip/README.md
@@ -1,18 +1,23 @@
-# linked-list-zip
+# README.md
 
-- Write a function called zipLists that takes two linked lists as arguments and returns a new linked list that is created by alternating nodes between the two input linked lists.
+## linked-list-zip
+
+- The problem is to write a function that takes in two linked lists and returns a new linked list that is the result of "zipping" the two input lists together. The nodes of the new list should alternate between the nodes of the two input lists, starting with the first node of the first list, then the first node of the second list, then the second node of the first list, and so on.
 
 ## Whiteboard Process
 
-- ![alt text](image_file_path.png)
+- 
 
 ## Approach & Efficiency
 
-- Big O
+- This algorithm uses a while loop to iterate through both input lists, adding nodes to the new list one by one. It keeps track of the head and tail of the new list using two variables, and adds any remaining nodes from one of the input lists to the new list at the end.
 
-Time: O(n), where n is the length of the longest linked list
-Space: O(1)
+## Big O
+
+- The time complexity of this algorithm is O(n), where n is the length of the longer input list. This is because we need to iterate through each node of the longer list in order to create the new list. The space complexity of this algorithm is O(1), because we only need to create a few extra variables to keep track of the head and tail of the new list.
 
 ## Solution
 
-[filename.py](path/to/filename.py)
+- [linked-list-zip](linked-list-zip.py)
+
+- [Code Challenge 08 readme](https://github.com/jdutchfoy/data-structures-and-algorithms/blob/main/python/code_challenges/linked-list-zip/README.md)
--- python/code_challenges/linked-list-zip/linked_list_zip.py
+++ python/code_challenges/linked-list-zip/linked_list_zip.py
@@ -1,42 +1,32 @@
-class Node:
-    def __init__(self, value=None, next_node=None):
-        self.value = value
-        self.next_node = next_node
-
-class LinkedList:
-    def __init__(self, head=None):
-        self.head = head
-
-    def zip_lists(self, other):
-        """Zip two linked lists together into one."""
-        if not self.head:
-            return other
-        if not other.head:
-            return self
-
-        current_self = self.head
-        current_other = other.head
-
-        while current_self and current_other:
-            # Save next nodes before rewiring
-            next_self = current_self.next_node
-            next_other = current_other.next_node
-
-            # Rewire current nodes
-            current_self.next_node = current_other
-            current_other.next_node = next_self
-
-            # Move to next nodes
-            current_self = next_self
-            current_other = next_other
-
-        return self
-
-    def __str__(self):
-        """Return a string representation of the linked list."""
-        output = ''
-        current = self.head
-        while current:
-            output += f'{{ {str(current.value)} }} -> '
-            current = current.next_node
-        return output + 'NULL'
+def zip_lists(list1, list2):
+    head = None
+    tail = None
+    
+    while list1 and list2:
+        if not head:
+            head = list1
+            tail = head
+            list1 = list1.next
+        else:
+            tail.next = list2
+            tail = tail.next
+            list2 = list2.next
+        
+        if list1:
+            tail.next = list1
+            tail = tail.next
+            list1 = list1.next
+    
+    if list1:
+        if tail:
+            tail.next = list1
+        else:
+            head = list1
+    
+    if list2:
+        if tail:
+            tail.next = list2
+        else:
+            head = list2
+    
+    return head
